6e6857fc6e40d61e968ccaf59a4920d528555359
cassieluken <40001862+cassieluken@users.noreply.github.com>
2020-11-18 10:38:10 -0800

    Update dbconnect.js
    Took out my password

--- dbconnect.js
+++ dbconnect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const uri = "mongodb+srv://cassie:cassie123@first-cluster.iqjzj.mongodb.net/tcsdb?retryWrites=true&w=majority";
+const uri = "mongodb+srv://cassie:<password>@first-cluster.iqjzj.mongodb.net/tcsdb?retryWrites=true&w=majority";
 mongoose.connect(uri,{ useUnifiedTopology: true },(err)=>{
 console.log('connected')
 
